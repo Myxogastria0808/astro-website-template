2ac647e5081177960fa3d526c5b8b61f8bcb9a8a
Yuki Osada <78744619+Myxogastria0808@users.noreply.github.com>
2025-07-12 18:37:58 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "natsuiro-website";
+  description = "astro website template";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
